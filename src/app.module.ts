6ebed182dc056229e39488d470bd18d0ad78b00b
Itzli Molina <itzli2000@msn.com>
2025-06-28 15:58:28 -0600

    feat(config): add environment configuration and PostgreSQL integration
    - Introduced .env.template for environment variable setup.
    - Updated .gitignore to include Yarn Modern files.
    - Added @nestjs/config and @nestjs/typeorm dependencies for configuration and database integration.
    - Configured TypeOrmModule in AppModule for PostgreSQL connection using environment variables.
    
    This commit enhances the application by enabling environment-based configuration and integrating PostgreSQL as the database solution.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true, // TODO: false in production
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
