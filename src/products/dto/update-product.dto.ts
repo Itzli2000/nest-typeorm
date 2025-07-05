5af84f6edf13150af9475c8f6eae5f20fa8e88f7
Itzli Molina <itzli2000@msn.com>
2025-07-05 12:30:33 -0600

    feat(products): add tags field to product DTOs and entity
    - Enhanced CreateProductDto to include an optional tags field with validation.
    - Updated Product entity to store tags as an array in the database.
    - Simplified UpdateProductDto by extending CreateProductDto without additional fields.
    
    This commit improves product management by allowing the categorization of products through tags, enhancing searchability and organization.

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,43 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
-import {
-  IsArray,
-  IsIn,
-  IsInt,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-  IsString,
-  MinLength,
-} from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {
-  @IsString()
-  @MinLength(1)
-  title: string;
-
-  @IsNumber()
-  @IsPositive()
-  @IsOptional()
-  price?: number;
-
-  @IsString()
-  @IsOptional()
-  description?: string;
-
-  @IsString()
-  @IsOptional()
-  slug?: string;
-
-  @IsInt()
-  @IsPositive()
-  @IsOptional()
-  stock?: number;
-
-  @IsString({ each: true })
-  @IsArray()
-  sizes: string[];
-
-  @IsIn(['men', 'women', 'kid', 'unisex'])
-  gender: string;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
