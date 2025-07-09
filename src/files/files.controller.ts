4f04834f207df415978f14d0f9c729faf5d56482
Itzli Molina <itzli2000@msn.com>
2025-07-08 19:27:20 -0600

    feat(files): add image retrieval functionality and environment variables
    - Implemented a new endpoint in FilesController to retrieve product images by name, enhancing image management capabilities.
    - Updated FilesService to include logic for checking image existence and returning the correct file path.
    - Added new environment variables (PORT and HOST_API) to the .env.template for better configuration management.
    - Integrated ConfigModule to access environment variables within the application.
    
    These changes improve the application's ability to serve product images dynamically, enhancing user experience and flexibility in configuration.

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,18 +1,35 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFilter, fileNamer } from './helpers';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.findProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -29,10 +46,10 @@ export class FilesController {
       throw new BadRequestException('File is required');
     }
 
-    console.log(file);
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
 
     return {
-      filename: file.originalname,
+      secureUrl,
     };
   }
 }
