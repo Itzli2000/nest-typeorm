283fa842830c6a0a4a540be9b40652c97b47cc4e
Itzli Molina <itzli2000@msn.com>
2025-07-11 08:15:40 -0600

    feat(auth): integrate JWT and Passport for authentication
    - Added JwtModule and PassportModule to the AuthModule for handling JWT-based authentication.
    - Configured JWT secret and expiration options to enhance security during user authentication.
    - Updated the imports in auth.module.ts for better organization and clarity.
    
    These changes improve the authentication mechanism by enabling JWT support, allowing for more secure and scalable user authentication.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '2h' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [TypeOrmModule],
